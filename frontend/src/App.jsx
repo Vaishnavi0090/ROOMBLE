179c21a953c183f756223eca1a039f7a63b7e199
Shlok Jain <85099963+Shlok-Jain@users.noreply.github.com>
2025-02-08 15:37:39 +0530

    Added Navbar design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { socket } from './socket'
+import { use } from 'react';
 
 function App() {
+  useEffect(()=>{
+    function handleConnection(){
+      console.log('a user connected');
+    }
+    socket.on('connect', handleConnection)
+  },[]);
+
+  //make a function to handle the connection event
+  //useEffect to listen to the connection event
   useEffect(()=>{
     function handleConnection(){
       console.log('a user connected');
