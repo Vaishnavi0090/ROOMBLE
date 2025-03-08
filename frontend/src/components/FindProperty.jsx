8de67e0c7961e488afd8053c4bfe7845c123cc3b
pranaysriharsha <pranaysurepally@gmail.com>
2025-03-08 18:14:03 +0530

    Find Property Initial Version

--- frontend/src/components/FindProperty.jsx
+++ frontend/src/components/FindProperty.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import SearchArea from "./FindPropertyComponents/SeachArea";
+import "../css/FindPropertyStyles/FindProperty.css";
 
 function FindProperty() {
     return (
-        <div>
-        <h1>Find Property</h1>
+        <div className="find-property-body">
+        <div className="search-area-div">
+            <SearchArea />
+        </div>
+        <div className="Property-card-div">
+            <h1>Property Cards</h1>
+        </div>
         </div>
     );
 }
