891677946918c392ca0d7575a76a5e979decbc0e
Hitarth <169062490+Hitarth-7@users.noreply.github.com>
2025-03-11 00:10:45 +0530

    OTP Page added for testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,44 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { socket } from './socket'
-import { Navbar } from './components/Navbar';
-import BaseState from './context/base/Basestate.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route } from 'react-router-dom';
-import Messages from './components/Messages';
-import SignUpPage from './components/SignUp';
-import Login from './components/Login.jsx';
-import AddProperty from './components/AddProperty.jsx';
-import FindProperty from './components/FindProperty.jsx';
-import FindFlatmate from './components/FindFlatmate.jsx';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { socket } from "./socket";
+import { Navbar } from "./components/Navbar";
+import BaseState from "./context/base/Basestate.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+import Messages from "./components/Messages";
+import SignUpPage from "./components/SignUp";
+import Login from "./components/Login.jsx";
+import AddProperty from "./components/AddProperty.jsx";
+import FindProperty from "./components/FindProperty.jsx";
+import FindFlatmate from "./components/FindFlatmate.jsx";
+import OTPPage from "./components/OTPPage/OTPPage.jsx";
 function App() {
-  useEffect(()=>{
-    function handleConnection(){
-      console.log('a user connected');
+  const [id, setID] = useState("");
+  useEffect(() => {
+    function handleConnection() {
+      console.log("a user connected");
     }
-    socket.on('connect', handleConnection)
-    socket.emit('join', socket.id);           //TODO: send user id here
-    socket.on('online_users', (data) => {
+    socket.on("connect", handleConnection);
+    socket.emit("join", socket.id); //TODO: send user id here
+    socket.on("online_users", (data) => {
       console.log(data);
     });
-    
-  },[]);
+  }, []);
 
   return (
     <BaseState>
-     <Navbar />
+      <Navbar />
       <Routes>
         <Route path="/messages" element={<Messages />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signup" element={<SignUpPage setID={setID} />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/add-property" element={<AddProperty/>}/>
-        <Route path="/find-property" element={<FindProperty/>} />
-        <Route path="/find-flatmate" element={<FindFlatmate/>}/>
+        <Route path="/add-property" element={<AddProperty />} />
+        <Route path="/find-property" element={<FindProperty />} />
+        <Route path="/find-flatmate" element={<FindFlatmate />} />
+        <Route path="/otp-page" element={<OTPPage id={id} />} />
       </Routes>
-      
     </BaseState>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -1,229 +1,364 @@
-import { responsiveFontSizes } from '@mui/material';
-import React from 'react';
-import {useState} from 'react';
-import config from '../config';
+import { responsiveFontSizes } from "@mui/material";
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import config from "../config";
 
-function SignUpForm(){
+function SignUpForm({ setID }) {
+  const navigate = useNavigate();
+  const [formInput, setFormInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    locality: "",
+    smoke: null,
+    pets: null,
+    veg: null,
+    flatmate: null,
+    gender: null,
+    successMsg: "",
+  });
+  const [formError, setFormError] = useState({
+    password: "",
+    confirmPassword: "",
+  });
 
-    const [formInput, setFormInput] = useState({
-      name: "",
-      email: "",
-      password: "",
+  const handleUserInput = (name, value) => {
+    setFormInput((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const validateFormInput = async (event) => {
+    event.preventDefault();
+    let inputError = {
       confirmPassword: "",
-      locality: "",
-      smoke: null,
-      pets: null,
-      veg:null,
-      flatmate: null,
-      gender: null,
-      successMsg: "",
-    });
-    const [formError, setFormError] = useState({
-      password: "",
-      confirmPassword: "",
-    });
-
-    const handleUserInput = (name, value) => {
-      setFormInput((prev) => ({...prev, [name]:value,}));
+    };
+    if (formInput.password.length < 6) {
+      setFormError({
+        ...inputError,
+        password: "Password should be atleast 6 characters",
+      });
+      // setFormInput({...formInput, successMsg: "",})
+      return;
     }
 
-    const validateFormInput = async (event) => {
-      event.preventDefault();
-      let inputError = {
-        confirmPassword: "",
-      };
-      if(formInput.password.length < 6){
-        setFormError({...inputError, password: "Password should be atleast 6 characters"});
-        // setFormInput({...formInput, successMsg: "",})
-        return;
-      }
-      
-      if(formInput.password.length > 10){
-        setFormError({...inputError, password: "Password should not be more than 10 characters"});
-        // setFormInput({...formInput, successMsg: "",})
-        return;
-      }
-      if(formInput.password !== formInput.confirmPassword){
-        setFormError({...inputError, confirmPassword: "Password and Confirm password do not match!"});
-        // setFormInput({...formInput, successMsg: "",})
-        return;
-      }
-      // setFormError(inputError);
-      // setFormInput((prevState) => ({
-      //   ...prevState, successMsg:"Validation Successful",
-      // }));
-      await sendDataToAPI();
+    if (formInput.password.length > 10) {
+      setFormError({
+        ...inputError,
+        password: "Password should not be more than 10 characters",
+      });
+      // setFormInput({...formInput, successMsg: "",})
+      return;
+    }
+    if (formInput.password !== formInput.confirmPassword) {
+      setFormError({
+        ...inputError,
+        confirmPassword: "Password and Confirm password do not match!",
+      });
+      // setFormInput({...formInput, successMsg: "",})
+      return;
+    }
+    // setFormError(inputError);
+    // setFormInput((prevState) => ({
+    //   ...prevState, successMsg:"Validation Successful",
+    // }));
+    await sendDataToAPI();
+  };
+
+  const sendDataToAPI = async () => {
+    const apiURL = "http://127.0.0.1:3000/authenticateTenant/Tenant_register";
+
+    const requestData = {
+      name: formInput.name,
+      email: formInput.email,
+      password: formInput.password,
+      locality: formInput.locality,
+      smoke: formInput.smoke === "yes",
+      pets: formInput.pets === "yes",
+      veg: formInput.veg === "yes",
+      flatmate: formInput.flatmate === "yes",
+      gender: formInput.gender === "male",
     };
 
-    const sendDataToAPI = async () => {
-     const apiURL = `${config.backend}/{look for the link to the signup route from backend}`;
-
-     const requestData = {
-      name:formInput.name,
-      email:formInput.email,
-      password:formInput.password,
-      locality:formInput.locality,
-      smoke:formInput.smoke==="yes",
-      pets:formInput.pets==="yes",
-      veg:formInput.veg==="yes",
-      flatmate:formInput.flatmate==="yes",
-      gender:formInput.gender==="male",
-     };
-
-     try {
-      const response = await fetch(apiURL, {method: "POST", headers: {"Content-Type": "application/json",}, body: JSON.stringify(requestData),});
+    try {
+      const response = await fetch(apiURL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(requestData),
+      });
 
       const responseData = await response.json();
 
-      if(responseData.success) {
-        setFormInput((prev)=>({...prev, successMsg: responseData.message}))
+      if (responseData.success) {
+        setFormInput((prev) => ({ ...prev, successMsg: responseData.message }));
+        setID(responseData.message);
+        navigate("/otp-page");
+        // navigate("/otp-page", { id: successMsg })
+      } else {
+        setFormInput((prev) => ({ ...prev, successMsg: responseData.message }));
+        // setID(responseData.message);
+        // navigate("/otp-page", { id: successMsg })
       }
-      else{
-        setFormInput((prev)=>({...prev, successMsg: responseData.message}))
-      }
-     }
-     catch (error) {
+    } catch (error) {
       console.error("Error sending data:", error);
       setFormInput((prev) => ({
         ...prev,
-        successMsg: "Couldn't fetch data",
-        
+        successMsg: "Couldn't fetch data.",
       }));
+      // setID(null);
     }
-    };
-
-
-
-    return (
-      <div className="signup-box">
-        <h2 className="title">Signup as a Tenant</h2>
-        
-        <form className="signup-form" onSubmit={validateFormInput}>
-        <div className="scroll-div">  
-          <div>
-          <label>Your good name</label>
-          <input type="text" className="input-box" placeholder="name" required name='name' onChange={({ target }) => handleUserInput(target.name, target.value)}/>
-          </div>
-          
-          <div>
-          <label>Your Email (please check regularly)</label>
-          <input
-            type="email"
-            className="input-box"
-            placeholder="mail@abc.com"
-            name='email'
-            onChange={({ target }) => handleUserInput(target.name, target.value)}
-            required
-          />
-          </div>
-          
-          <div>
-          <label>Password</label>
-          <input
-            type="password"
-            className="input-box"
-            placeholder="*************"
-            name="password"
-            value={formInput.password}
-            onChange={({target})=>{handleUserInput(target.name, target.value)}}
-            required
-          />
-          <p className='error-password'>{formError.password}</p>
-          </div>
-          
-          <div>
-          <label>Confirm Password</label>
-          <input
-            type="password"
-            className="input-box"
-            placeholder="*************"
-            name="confirmPassword"
-            value={formInput.confirmPassword}
-            onChange={({target})=>{handleUserInput(target.name, target.value)}}
-            required
-          />
-          <p className='error-confirm-password'>{formError.confirmPassword}</p>
-          <p className='success-message'>{formInput.successMsg}</p>
-          
-          
-          
-          <p className="question">A few questions about you :</p>
-          <label>
-            Where would you like to look for a property?
-            <br />
-            (For better recommendations)
-          </label>
-          <input
-            type="text"
-            className="input-box"
-            placeholder="Eg. BanyanVilla Road"
-            name='locality'
-            onChange={({target})=>{handleUserInput(target.name, target.value)}}
-            required
-          />
-          </div>
-          
-          
-          <div className="options">
-                 <label>
-        Do you smoke/drink?
-        <label htmlFor="smoke-yes">
-          <input type="radio" id="smoke-yes" name="smoke" className="radio" value="yes" required onChange={({target})=>{handleUserInput(target.name, target.value)}} /> Yes
-        </label>
-        <label htmlFor="smoke-no">
-          <input type="radio" id="smoke-no" name="smoke" className="radio" value="no" onChange={({target})=>{handleUserInput(target.name, target.value)}} /> No
-        </label>
-      </label>
-
-      <label>
-        Do you plan on keeping pets?
-        <label htmlFor="pets-yes">
-          <input type="radio" id="pets-yes" name="pets" className="radio" value="yes" required onChange={({target})=>{handleUserInput(target.name, target.value)}}/> Yes
-        </label>
-        <label htmlFor="pets-no">
-          <input type="radio" id="pets-no" name="pets" className="radio" value="no" onChange={({target})=>{handleUserInput(target.name, target.value)}}/> No
-        </label>
-      </label>
-
-      <label>
-        Do you eat Non-veg?
-        <label htmlFor="veg-yes">
-          <input type="radio" id="veg-yes" name="veg" className="radio" value="yes" required onChange={({target})=>{handleUserInput(target.name, target.value)}}/> Yes
-        </label>
-        <label htmlFor="veg-no">
-          <input type="radio" id="veg-no" name="veg" className="radio" value="no" onChange={({target})=>{handleUserInput(target.name, target.value)}} /> No
-        </label>
-      </label>
-
-      <label>
-        Do you need a flatmate?
-        <label htmlFor="flatmate-yes">
-          <input type="radio" id="flatmate-yes" name="flatmate" className="radio" value="yes" required onChange={({target})=>{handleUserInput(target.name, target.value)}}/> Yes
-        </label>
-        <label htmlFor="flatmate-no">
-          <input type="radio" id="flatmate-no" name="flatmate" className="radio" value="no" onChange={({target})=>{handleUserInput(target.name, target.value)}}/> No
-        </label>
-      </label>
-
-      <label>
-        What's your gender?
-        <label htmlFor="gender-male">
-          <input type="radio" id="gender-male" name="gender" className="radio" value="male" required onChange={({target})=>{handleUserInput(target.name, target.value)}}/> Male
-        </label>
-        <label htmlFor="gender-female">
-          <input type="radio" id="gender-female" name="gender" className="radio" value="female" onChange={({target})=>{handleUserInput(target.name, target.value)}}/> Female
-        </label>
-      </label>
-          </div>
-          </div>
-          <button className="signup-button">Sign up</button>
-        </form>
-        
-        <p className="footer-text-signup">
-          With Roomble, you'll stumble on the perfect place to rumble!
-        </p>
-      </div>
-    );
   };
-  export default SignUpForm;
-  
+
+  return (
+    <div className="signup-box">
+      <h2 className="title">Signup as a Tenant</h2>
+
+      <form className="signup-form" onSubmit={validateFormInput}>
+        <div className="scroll-div">
+          <div>
+            <label>Your good name</label>
+            <input
+              type="text"
+              className="input-box"
+              placeholder="name"
+              required
+              name="name"
+              onChange={({ target }) =>
+                handleUserInput(target.name, target.value)
+              }
+            />
+          </div>
+
+          <div>
+            <label>Your Email (please check regularly)</label>
+            <input
+              type="email"
+              className="input-box"
+              placeholder="mail@abc.com"
+              name="email"
+              onChange={({ target }) =>
+                handleUserInput(target.name, target.value)
+              }
+              required
+            />
+          </div>
+
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              className="input-box"
+              placeholder="*************"
+              name="password"
+              value={formInput.password}
+              onChange={({ target }) => {
+                handleUserInput(target.name, target.value);
+              }}
+              required
+            />
+            <p className="error-password">{formError.password}</p>
+          </div>
+
+          <div>
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              className="input-box"
+              placeholder="*************"
+              name="confirmPassword"
+              value={formInput.confirmPassword}
+              onChange={({ target }) => {
+                handleUserInput(target.name, target.value);
+              }}
+              required
+            />
+            <p className="error-confirm-password">
+              {formError.confirmPassword}
+            </p>
+            <p className="success-message">{formInput.successMsg}</p>
+
+            <p className="question">A few questions about you :</p>
+            <label>
+              Where would you like to look for a property?
+              <br />
+              (For better recommendations)
+            </label>
+            <input
+              type="text"
+              className="input-box"
+              placeholder="Eg. BanyanVilla Road"
+              name="locality"
+              onChange={({ target }) => {
+                handleUserInput(target.name, target.value);
+              }}
+              required
+            />
+          </div>
+
+          <div className="options">
+            <label>
+              Do you smoke/drink?
+              <label htmlFor="smoke-yes">
+                <input
+                  type="radio"
+                  id="smoke-yes"
+                  name="smoke"
+                  className="radio"
+                  value="yes"
+                  required
+                  onChange={({ target }) => {
+                    handleUserInput(target.name, target.value);
+                  }}
+                />{" "}
+                Yes
+              </label>
+              <label htmlFor="smoke-no">
+                <input
+                  type="radio"
+                  id="smoke-no"
+                  name="smoke"
+                  className="radio"
+                  value="no"
+                  onChange={({ target }) => {
+                    handleUserInput(target.name, target.value);
+                  }}
+                />{" "}
+                No
+              </label>
+            </label>
+
+            <label>
+              Do you plan on keeping pets?
+              <label htmlFor="pets-yes">
+                <input
+                  type="radio"
+                  id="pets-yes"
+                  name="pets"
+                  className="radio"
+                  value="yes"
+                  required
+                  onChange={({ target }) => {
+                    handleUserInput(target.name, target.value);
+                  }}
+                />{" "}
+                Yes
+              </label>
+              <label htmlFor="pets-no">
+                <input
+                  type="radio"
+                  id="pets-no"
+                  name="pets"
+                  className="radio"
+                  value="no"
+                  onChange={({ target }) => {
+                    handleUserInput(target.name, target.value);
+                  }}
+                />{" "}
+                No
+              </label>
+            </label>
+
+            <label>
+              Do you eat Non-veg?
+              <label htmlFor="veg-yes">
+                <input
+                  type="radio"
+                  id="veg-yes"
+                  name="veg"
+                  className="radio"
+                  value="yes"
+                  required
+                  onChange={({ target }) => {
+                    handleUserInput(target.name, target.value);
+                  }}
+                />{" "}
+                Yes
+              </label>
+              <label htmlFor="veg-no">
+                <input
+                  type="radio"
+                  id="veg-no"
+                  name="veg"
+                  className="radio"
+                  value="no"
+                  onChange={({ target }) => {
+                    handleUserInput(target.name, target.value);
+                  }}
+                />{" "}
+                No
+              </label>
+            </label>
+
+            <label>
+              Do you need a flatmate?
+              <label htmlFor="flatmate-yes">
+                <input
+                  type="radio"
+                  id="flatmate-yes"
+                  name="flatmate"
+                  className="radio"
+                  value="yes"
+                  required
+                  onChange={({ target }) => {
+                    handleUserInput(target.name, target.value);
+                  }}
+                />{" "}
+                Yes
+              </label>
+              <label htmlFor="flatmate-no">
+                <input
+                  type="radio"
+                  id="flatmate-no"
+                  name="flatmate"
+                  className="radio"
+                  value="no"
+                  onChange={({ target }) => {
+                    handleUserInput(target.name, target.value);
+                  }}
+                />{" "}
+                No
+              </label>
+            </label>
+
+            <label>
+              What's your gender?
+              <label htmlFor="gender-male">
+                <input
+                  type="radio"
+                  id="gender-male"
+                  name="gender"
+                  className="radio"
+                  value="male"
+                  required
+                  onChange={({ target }) => {
+                    handleUserInput(target.name, target.value);
+                  }}
+                />{" "}
+                Male
+              </label>
+              <label htmlFor="gender-female">
+                <input
+                  type="radio"
+                  id="gender-female"
+                  name="gender"
+                  className="radio"
+                  value="female"
+                  onChange={({ target }) => {
+                    handleUserInput(target.name, target.value);
+                  }}
+                />{" "}
+                Female
+              </label>
+            </label>
+          </div>
+        </div>
+        <button className="signup-button">Sign up</button>
+      </form>
+
+      <p className="footer-text-signup">
+        With Roomble, you'll stumble on the perfect place to rumble!
+      </p>
+    </div>
+  );
+}
+export default SignUpForm;
