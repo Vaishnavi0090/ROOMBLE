847901c489a20faa4258e05ec5893f7014a9d2e4
bikkygill05 <bsingh23@iitk.ac.in>
2025-03-20 23:46:17 +0530

    bug fix

--- backend/models/Tenant.js
+++ backend/models/Tenant.js
@@ -24,7 +24,7 @@ const TenantSchema = new mongoose.Schema({
     },
     city: {
         type : String,
-        required : true,
+        required : false,
     },
     gender: {// false female,true male
         type: Boolean,
